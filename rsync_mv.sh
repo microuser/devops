03681fa6bba314a6e32a1d039f672718188c9fbe
MicroUser <microuser@users.noreply.github.com>
2016-06-17 15:48:02 -0400

    Update rsync_mv.sh

--- rsync_mv.sh
+++ rsync_mv.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
 if [ -z "$RSYNC_MV_OPTS" ]
-  RSYNC_MV_OPTS="-a -P --remove-source-files"
+  RSYNC_MV_OPTS="-a -P"
+  
+  ##For some version for RSYNC
+  RSYNC_MV_MV_CMD="--remove-source-files"
+  ##For other versions of rsync
+  #RSYNC_MV_MV_CMD="--remove-sent-files" #some documentation says --delete-sent-files
+  ##TODO: Add rsync feature detection or version detection
+  
 fi
 
-if rsync $RSYNC_MV_OPTS $@
+rsync $RSYNC_MV_OPTS $@
+echo "Rsync frist pass complete, Running again while removing source files"
+if rsync $RSYNC_MV_OPTS $RSYNC_MV_MV_CMD $@
 then
+  echo "Rsync second pass complete. Removing Empty Directories"
   ## Iterate so rsync_mv behaves with mutiple sources
   while [ ! -z "$2" ]
-
+  do
   ##Remove Empty Directories, which should be empty after rsync
     find "$1" -type d -empty -delete
 
   ##Shift the parameters left, so "$3" becomes "$2" and "$2" becomes "$1"
   shift
-
+  done
+  exit 0;
 else
   echo ERROR: Failed Command: rsync -a -P --remove-source-files $@
   echo No proceding to remove old directory
+  exit 1;
 fi
+
+
