e968a7fdae2da29aac3a9888034fa0a0b23980e1
MicroUser <microuser@users.noreply.github.com>
2019-01-22 22:44:48 -0500

    Update regex.sh

--- regex.sh
+++ regex.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-
-exit 0
+function regex { 
+	if [ "$1" == "-f" ] ; then
+		# test with ...
+		# echo '#!#hello#=#world#;#' > $0.mem
+		# regex -f $0.mem '.*#!#(.+)#=#(.+)#;#.*' 2
+		#
+		cat "$2" | gawk 'match($0,/'$3'/, ary) {print ary['${4:-'0'}']}' | head -n 1; 
+	else
+		# test with...
+		#echo '#!#hello#=#world#;#' | regex '.*#!#(.+)#=#(.+)#;#.*' 2
+		#
+		gawk 'match($0,/'$1'/, ary) {print ary['${2:-'0'}']}' | head -n 1; 
+	fi
+}
